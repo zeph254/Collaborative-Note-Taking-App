11aeefc88846eff8cba819565db68d1839491dff
zeph254 <ularezephaniah@gmail.com>
2025-02-01 12:32:44 +0300

    deployment

--- backend/models.py
+++ backend/models.py
@@ -10,7 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     role = db.Column(db.String(50), default='collaborator')  # Role: admin or collaborator
     notes_created = db.relationship('Note', backref='creator', lazy=True)
     notes_shared = db.relationship('Note', secondary='shared_notes', back_populates='shared_with')
